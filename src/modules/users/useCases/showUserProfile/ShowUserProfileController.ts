4a337993245f829335a044522acd1e7ca0356b79
Leonardo Lopes <leonardolopesb97@hotmail.com>
2022-02-05 23:56:22 -0300

    feat: :rocket: Challenge finished

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,13 +1,22 @@
-import { Request, Response } from "express";
-
-import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+import { Request, Response } from 'express'
+import { User } from 'modules/users/model/User'
+import { ShowUserProfileUseCase } from './ShowUserProfileUseCase'
 
 class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params
+
+    let user: User
+
+    try {
+      user = this.showUserProfileUseCase.execute({ user_id })
+      return response.status(200).json(user)
+    } catch (error) {
+      return response.status(404).json({ error: error.message })
+    }
   }
 }
 
-export { ShowUserProfileController };
+export { ShowUserProfileController }
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,13 +1,22 @@
-import { Request, Response } from "express";
-
-import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+import { Request, Response } from 'express'
+import { User } from 'modules/users/model/User'
+import { TurnUserAdminUseCase } from './TurnUserAdminUseCase'
 
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params
+
+    let user: User
+
+    try {
+      user = this.turnUserAdminUseCase.execute({ user_id })
+      return response.status(200).json(user)
+    } catch (error) {
+      return response.status(404).json({ error: error.message })
+    }
   }
 }
 
-export { TurnUserAdminController };
+export { TurnUserAdminController }
